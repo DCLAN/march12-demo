af5e662acfa892f3b67f2a99952f257e09c79b8d
Daniel Lanthier <dlanthier@circleci.com>
2021-05-12 09:45:25 -0400

    Breaking build

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello world, it is May 12th
         <a
           className="App-link"
           href="https://reactjs.org"
